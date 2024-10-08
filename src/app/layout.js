73d90596220a93d7540cf292a6e8b1d362a1d357
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-10-08 11:30:04 -0300

    Se agregó en tailwind.config.js el plugin de Flowbite y se agregó el Script del mismo en layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="../path/to/flowbite/dist/flowbite.min.js"></Script>
       </body>
     </html>
   );
