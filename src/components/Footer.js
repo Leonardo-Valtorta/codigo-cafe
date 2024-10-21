5c3619f1e387573a49200acae2999d48a9de98a2
CarolinaKegalj <carokegalj@gmail.com>
2024-10-21 10:20:07 -0300

    .

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+const Footer = () => {
+    return (
+
+<footer class="bg-[#15605b] rounded-lg shadow m-4 ">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white -400 sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Nosotros</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Locales</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Productos</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contacto</a>
+        </li>
+    </ul>
+    <span class="text-sm text-white sm:text-center hover:underline">Todos los derechos reservados.
+    </span>
+    </div>
+</footer>
+    )
+      
+}
+export default Footer
+
