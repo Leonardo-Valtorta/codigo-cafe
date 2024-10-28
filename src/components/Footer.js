77bfc90fd257e95c7905974f9fab5a78252f98b8
CarolinaKegalj <carokegalj@gmail.com>
2024-10-28 20:13:21 -0300

    Responsividad del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 <footer class="bg-[#15605b] rounded-lg shadow m-4 ">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white -400 sm:mt-0">
+    <ul class="sm:flex justify-center mt-3 text-sm font-medium text-white sm:mt-0">
         <li>
             <a href="#" class="hover:underline me-4 md:me-6">Nosotros</a>
         </li>
@@ -18,7 +18,7 @@ const Footer = () => {
             <a href="#" class="hover:underline">Contacto</a>
         </li>
     </ul>
-    <span class="text-sm text-white sm:text-center hover:underline">Todos los derechos reservados.
+    <span class="sm: flex justify-center text-sm text-white sm:text-center">Todos los derechos reservados.
     </span>
     </div>
 </footer>
