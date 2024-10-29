91a5b954e257ef79161e0e161ef2f9e8f6536c4f
andre <andyalgarte@gmail.com>
2024-10-29 18:43:00 -0300

    Creo el componente ShoppingCard que representa un ítem en el carrito de compras

---
+++ src/components/cartSection/ShoppingCard.js
@@ -0,0 +1,32 @@
+import PillButton from "../buttons/PillButton";
+import IconButton from "../buttons/IconButton";
+import IconRemove from "../icons/IconRemove";
+import IconMore from "../icons/IconMore"; 
+import IconLess from "../icons/IconLess";
+
+const ShoppingCard = (props) =>{
+    return (<>
+  <div className="flex flex-col bg-bgShoppingCard p-2 shadow-xl my-4">
+    <h4 className="bg-Coffee p-2 font-bold">{props.item.descripcion}</h4>
+    <div className ="flex flex-row">
+      <div className="w-2/6 p-1">
+        <img src = {props.item.imagen} w-full object-contain/>
+      </div>
+      <div className= "flex flex-col justify-between w-4/6 p-2">
+        <p className="py-2">Subtotal: $ {props.item.precio}</p>
+        <div>
+            <IconButton icon = {<IconMore/>}/>
+            <span className="text-xl">{props.item.cantidad}</span>
+            <IconButton icon = {<IconLess/>}/>
+        </div>
+        <div className="self-end">
+        <PillButton className="bg-btnRemoveItem hover:bg-btnRemoveItemHover"><IconRemove/>Quitar</PillButton>
+        </div>
+      </div>
+    </div>
+{/* FIN CARD DE ITEM DE CARRITO */}     
+  </div>
+    </>)
+}
+
+export default ShoppingCard;
