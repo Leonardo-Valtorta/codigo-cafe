6d5564550cbfe8ba55ceb7d075514ca4f92f1cca
andre <andyalgarte@gmail.com>
2024-10-29 18:13:16 -0300

    Incorporo la fuente JOCKEY para títulos de secciones y configuro colores para el carrito en tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,21 @@ module.exports = {
         navbarBackground: "#F5D48E",
         navbarBackgroundSecond: "#f5c152",
         navbarBackgroundThird: "#edc368",
+
+        /* Colores Shopping Cart */
+        bgShoppingCart:"#D6A367",
         btnRemoveItem:"#FFB7CD",
         btnRemoveItemHover:"#F381A3",
-        btmDeleteAll:"#B3261E"
+        btnDeleteAll:"#B3261E",
+        btnDeleteAllHover:"#830c06",
         Coffee: "#B46C3C",
-        
+        bgShoppingCard:"#D9D9D9",
+        btnBuy:"#e67326",
+        btnBuyAllHover:"#f76d11"
       },
       fontFamily: {
         navbar: ["Pompiere", "sans-serif"],
+        titleCart:["Jockey One", "sans-serif"]
       },
     },
   },
