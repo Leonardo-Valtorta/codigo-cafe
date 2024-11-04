20b8bd550443f43a5f8e8efa1bb39a71c388b37c
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-04 12:24:35 -0300

    Se modificaron etiquetas <a> por <Link> y se modificó el orden z del nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 const Header = () => {
@@ -28,12 +29,12 @@ const Header = () => {
   );
 
   return (
-    <div className="sticky top-0 left-0 font-navbar text-xl bg-gradient-to-r from-navbarBackground via-navbarBackgroundThird to-navbarBackgroundSecond">
+    <div className="z-50 sticky top-0 left-0 font-navbar text-xl bg-gradient-to-r from-navbarBackground via-navbarBackgroundThird to-navbarBackgroundSecond">
       <div className="flex flex-col mx-0 sm:mx-4 sm:flex-row items-center px-4 py-2">
         <div className="sm:w-[30%] lg:w-1/4 w-full flex flex-row items-center">
-          <a href="/">
+          <Link href="/">
             <img src="logo-codigo-cafe.png" alt="Logo"/>
-          </a>
+          </Link>
 
           <button
             onClick={toggleNavbar}
@@ -46,16 +47,16 @@ const Header = () => {
         <div className={`w-full sm:flex sm:items-center sm:w-full ${isOpen ? 'block' : 'hidden'}`}>
           <ul className="flex flex-col py-3 px-0 items-start justify-evenly w-full sm:flex-row sm:items-center sm:w-full">
             <li className="my-2 sm:my-0 hover:underline">
-              <a href="/aboutUs" className="text-black px-3 py-2">NOSOTROS</a>
+              <Link href="/aboutUs" className="text-black px-3 py-2">NOSOTROS</Link>
             </li>
             <li className="my-2 sm:my-0 hover:underline">
-              <a href="/locales" className="text-black px-3 sm:pl-0 py-2">LOCALES</a>
+              <Link href="/locales" className="text-black px-3 sm:pl-0 py-2">LOCALES</Link>
             </li>
             <li className="my-2 sm:my-0 hover:underline">
-              <a href="#productos" className="text-black px-3 sm:pl-0 py-2">PRODUCTOS</a>
+              <Link href="#productos" className="text-black px-3 sm:pl-0 py-2">PRODUCTOS</Link>
             </li>
             <li className="my-2 sm:my-0 hover:underline">
-              <a href="#trabaja" className="text-black pl-3 sm:pl-0 py-2 text-center">TRABAJA CON NOSOTROS</a>
+              <Link href="#trabaja" className="text-black pl-3 sm:pl-0 py-2 text-center">TRABAJA CON NOSOTROS</Link>
             </li>
           </ul>
         </div>
@@ -63,16 +64,16 @@ const Header = () => {
         <div className={`w-full sm:justify-end sm:flex sm:items-center sm:w-full ${isOpen ? 'block' : 'hidden'}`}>
           <ul className="flex flex-col sm:px-0 py-3 px-3 items-start sm:flex-row sm:items-center justify-end w-full sm:w-auto">
             <li className="my-2 sm:my-0 flex items-center hover:underline">
-              <a className="w-6 h-auto" href="/contact">
+              <Link className="w-6 h-auto" href="/contact">
                 <img src="telefono-contacto.png" alt="Contacto" />
-              </a>
-              <a href="/contact" className="text-black pr-3 pl-2 py-2">Contacto</a>
+              </Link>
+              <Link href="/contact" className="text-black pr-3 pl-2 py-2">Contacto</Link>
             </li>
             <li className="my-2 sm:my-0 flex items-center hover:underline">
-              <a className="w-6 h-auto" href="/cart">
+              <Link className="w-6 h-auto" href="/cart">
                 <img src="carrito-tienda.png" alt="Tienda" />
-              </a>
-              <a href="/cart" className="text-black pr-3 pl-2 py-2">Tienda</a>
+              </Link>
+              <Link href="/cart" className="text-black pr-3 pl-2 py-2">Tienda</Link>
             </li>
           </ul>
         </div>
