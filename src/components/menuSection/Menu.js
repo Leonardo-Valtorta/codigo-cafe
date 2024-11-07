3e8d7deeb4368cf59bf448f7ffc62c3911064688
FranciscoMaiaru <franciscomaiaru98@gmail.com>
2024-11-07 17:19:33 -0300

    asd

--- src/components/menuSection/Menu.js
+++ src/components/menuSection/Menu.js
@@ -20,12 +20,22 @@ const Menu = () =>{
             <hr className="border-t-2 border-black my-3 mx-20"/> 
             <section id="otras-bebidas" className="text-wrap items-center">
                 <h1 className="font-medium italic text-[24px] text-center md:text-left">Otras Bebidas</h1>
-                <p>AAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                <div className= "flex flex-col justify-center">
+                    {products
+                        .filter(product=> product.category === "otras")
+                        .map (product => { return (<div><ProductCard key={product.id} product = {product} addToCart={addToCart}/><hr></hr></div>)})
+                    }
+                </div>
             </section>
             <hr className="border-t-2 border-black my-3 mx-20"/> 
             <section id="comida" className="text-wrap items-center">
                 <h1 className="font-medium italic text-[24px] text-center md:text-left">Comida</h1>
-                <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                <div className= "flex flex-col justify-center">
+                    {products
+                        .filter(product=> product.category === "comida")
+                        .map (product => { return (<div><ProductCard key={product.id} product = {product} addToCart={addToCart}/><hr></hr></div>)})
+                    }
+                </div>
             </section>
           
         </>) 
--- src/pages/products.js
+++ src/pages/products.js
@@ -16,7 +16,7 @@ export default function Products() {
         
         
         <div className="flex flex-col bg-cafeLight w-full pb-8 font-body">
-            <div className="sticky text-center bg-cafeLight w-full h-20 pb-4 md:h-30 grid grid-cols-3 font-navbar text-2xl font-bold">
+            <div className="top-1 sticky text-center bg-cafeLight w-full h-20 pb-4 md:h-30 grid grid-cols-3 font-navbar text-2xl font-bold">
                 <Link href="#cafes" className="p-4 border-2 hover:border-white hover:text-white px-3 sm:pl-0 py-2 hover:underline hover:bg-navbarBackgroundSecond">
                     CAFES
                 </Link>
