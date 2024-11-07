91fe3cf3986bce272649c90f6b391cd9717da7ed
FranciscoMaiaru <franciscomaiaru98@gmail.com>
2024-11-07 03:09:50 -0300

    Tarjeta product y base de datos json(sigue).

--- src/components/menuSection/ProductCard.js
+++ src/components/menuSection/ProductCard.js
@@ -2,24 +2,22 @@ import PillButton from "../buttons/PillButton";
 import IconButton from "../buttons/IconButton";
 
 
-const ProductCard = (props) =>{
+const ProductCard = (item,addToCart) =>{
+
+    const {name,price,image,description} = item;
+
     return (<>
         <div className="flex flex-col shadow-xl my-4">
-            <h3 className="text-[20px] font-light italic">{props.item.name}</h3>
-            <h4 className="text-[10px] font-light italic">{props.item.description}</h4>
+            <h3 className="text-[20px] font-medium italic">{name}</h3>
+            <h4 className="text-[10px] font-light italic">{description}</h4>
             <div className ="flex flex-row">
                 <div className="w-2/6 p-1">
-                    <img src = {props.item.image} w-full object-contain/>
+                    <img src = {image} w-full object-contain/>
                 </div>
                 <div className= "flex flex-col justify-between w-4/6 p-2">
-                    <p className="py-2">Subtotal: $ {props.item.price}</p>
-                    <div>
-                        <IconButton icon = {<IconMore/>}/>
-                        <span className="text-xl">{props.item.quantity}</span>
-                        <IconButton icon = {<IconLess/>}/>
-                    </div>
+                    <p className="py-2 font-bold-italic">Precio: $ {price}</p>
                     <div className="self-end">
-                    <PillButton className="bg-btnRemoveItem hover:bg-btnRemoveItemHover"><IconRemove/>Quitar</PillButton>
+                    <PillButton onClick = {() => addToCart(id)} className="bg-Btn-Agregar"><IconPlus/>Agregar al carrito</PillButton>
                     </div>
                 </div>
             </div>   
@@ -28,3 +26,4 @@ const ProductCard = (props) =>{
 }
 
 export default ProductCard;
+
