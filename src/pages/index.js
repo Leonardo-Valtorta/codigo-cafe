243d4a5214e583f9893310f44db19b2990488e1d
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-07 14:24:00 -0300

    Se arregraron problemas de fusión en index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,13 @@ import Footer from "@/components/Footer";
 import HomeSection from "@/components/HomeSection";
 
 
-const DynamicCarousel = dynamic(() => import('@/components/Carousel'), { ssr: false });
-
-const Home = () => {
+// const DynamicCarousel = dynamic(() => import('@/components/Carousel'), { ssr: false });
+export default function Home() {
   return (
     <>
       <Header />
       <HomeSection />
       <Footer />
-    </>     
+    </>
   );
 }
