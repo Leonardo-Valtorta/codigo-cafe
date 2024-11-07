2e2d0244135bd3eb20d552431224f24b882a55f6
alejosampa <alejo_sampa28@outlook.com>
2024-11-07 00:01:48 -0300

    Creo section home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import HomeSection from "@/components/HomeSection";
+
+
 
 
 export default function Home() {
   return (
     <>
-    
-    </>
+      <Header />
+      <HomeSection />
+      <Footer />
+    </>     
   );
 }
