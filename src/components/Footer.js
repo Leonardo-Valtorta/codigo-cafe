7d88c128ba2ecd2f44c9e96eac905dc576a94b3c
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-07 16:05:35 -0300

    Se cambió class por className y se modificó el nombre del color verde.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,24 @@
 const Footer = () => {
     return (
 
-<footer class="bg-[#15605b] rounded-lg shadow m-4 ">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+<footer className="bg-verde rounded-lg shadow m-4 ">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       
-    <ul class="sm:flex justify-center mt-3 text-sm font-medium text-white sm:mt-0">
+    <ul className="sm:flex justify-center mt-3 text-sm font-medium text-white sm:mt-0">
         <li>
-            <a href="/aboutUs" class="hover:underline me-4 md:me-6">Nosotros</a>
+            <a href="/aboutUs" className="hover:underline me-4 md:me-6">Nosotros</a>
         </li>
         <li>
-            <a href="/locales" class="hover:underline me-4 md:me-6">Locales</a>
+            <a href="/locales" className="hover:underline me-4 md:me-6">Locales</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Productos</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Productos</a>
         </li>
         <li>
-            <a href="/contact" class="hover:underline">Contacto</a>
+            <a href="/contact" className="hover:underline">Contacto</a>
         </li>
     </ul>
-    <span class="sm: flex justify-center text-sm text-white sm:text-center">Todos los derechos reservados.
+    <span className="sm: flex justify-center text-sm text-white sm:text-center">Todos los derechos reservados.
     </span>
     </div>
 </footer>
