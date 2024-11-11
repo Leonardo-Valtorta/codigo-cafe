e9c3465731472842a1e79625320d0a49726e7d6f
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-11 09:32:59 -0300

    Se rediseñó la sección Home para que contenga los CardTurn

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,48 +1,51 @@
-import Link from 'next/link';
-import dynamic from 'next/dynamic';
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import CardTurn from "./CardTurn";
 
-const Carousel = dynamic(() => import('./Carousel'), { ssr: false });
+const Carousel = dynamic(() => import("./Carousel"), { ssr: false });
 
 const HomeSection = () => {
-    return (
-        <>
-            <main className="flex flex-col items-center justify-between bg-backgroundMain">
-              <Carousel />
-                <div className="flex flex-col w-full">
-                  <div className="flex flex-col w-full p-6">
-                    <h1 className="text-center font-semibold font-title text-[36px] mt-5 sm:text-[48px]">BIENVENIDOS</h1>
-                    <p className="text-start font-semibold font-title text-[20px] sm:text-[36px]">Conocé la experiencia Codigo Café</p>
-                  </div>
-                  <div className="flex flex-col sm:flex-row sm:py-1">
-                    <div className="flex flex-row justify-center w-full px-6 py-4 sm:justify-end">
-                      <div className="flex flex-col mx-4 my-2 bg-backgroundCafe bg-cover">
-                        <Link className="font-title font-bold text-white py-10 px-10" href="/aboutUs">
-                          NOSOTROS
-                        </Link>
-                      </div>
-                      <div className="flex flex-col mx-4 my-2 bg-backgroundLocal bg-cover">
-                        <Link className="font-title font-bold text-white py-10 px-10" href="/locales">
-                          LOCALES
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="flex flex-row justify-center w-full px-6 py-4 sm:justify-start">
-                      <div className="flex flex-col mx-4 my-2 bg-backgroundTostado bg-cover">
-                        <Link className="font-title font-bold text-white py-10 px-10" href="/products">
-                          PRODUCTOS
-                        </Link>
-                      </div>
-                      <div className="flex flex-col mx-4 my-2 bg-backgroundPersonal bg-cover">
-                        <Link className="font-title font-bold text-white py-10 px-10" href="/aboutUs">
-                          TRABAJA
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>  
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <main className="flex flex-col items-center justify-between bg-backgroundMain">
+        <Carousel />
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full p-6">
+            <h1 className="text-center font-semibold font-title text-[36px] mt-5 sm:text-[48px]">
+              BIENVENIDOS
+            </h1>
+            <p className="text-start font-semibold font-title text-[20px] sm:text-[36px]">
+              Conocé la experiencia Codigo Café
+            </p>
+          </div>
+          <div className="flex justify-center">
+          <div className="grid grid-cols-4 gap-4">
+              <CardTurn 
+                imagen="/cafe.jpg" 
+                pagina="/aboutUS" 
+                texto="NOSOTROS" 
+              />
+              <CardTurn 
+                imagen="/local.jpg" 
+                pagina="/locales" 
+                texto="LOCALES" 
+              />
+              <CardTurn
+                imagen="/tostado.jpg"
+                pagina="/products"
+                texto="PRODUCTOS"
+              />
+              <CardTurn
+                imagen="/personal.jpg"
+                pagina="/aboutUs"
+                texto="TRABAJA"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default HomeSection;
