bfcf32f3ff9c89982303ebdaa38b886b85e82bdf
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-13 17:38:05 -0300

    Se modificó transparencia y brillo a la imagen de fondo y se modificó el texto para una mejor lectura

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import dynamic from "next/dynamic";
 import CardTurn from "./CardTurn";
 
@@ -7,19 +6,20 @@ const Carousel = dynamic(() => import("./Carousel"), { ssr: false });
 const HomeSection = () => {
   return (
     <>
-      <main className="flex flex-col items-center justify-between bg-backgroundMain">
+      <main className="flex flex-col items-center justify-between">
         <Carousel />
-        <div className="flex flex-col w-full">
-          <div className="flex flex-col w-full p-6">
-            <h1 className="text-center font-semibold font-title text-[36px] mt-5 sm:text-[48px]">
+        <div className="relative w-full pb-5">
+          <div className="absolute inset-0 bg-backgroundMain opacity-60"></div>
+          <div className="relative z-10 flex flex-col w-full p-4">
+            <h1 className="text-center font-extrabold drop-shadow-lg text-[36px] sm:text-[48px] opacity-100 blur-0">
               BIENVENIDOS
             </h1>
-            <p className="text-start font-semibold font-title text-[20px] sm:text-[36px]">
+            <p className="text-start font-semibold font-title text-[20px] sm:text-[36px] opacity-100">
               Conocé la experiencia Codigo Café
             </p>
           </div>
-          <div className="flex justify-center">
-          <div className="grid xl:grid-cols-4 gap-4 md:grid-cols-2">
+          <div className="flex justify-center pt-3">
+            <div className="grid xl:grid-cols-4 gap-4 md:grid-cols-2 opacity-100">
               <CardTurn 
                 imagen="/cafe.jpg" 
                 pagina="/aboutUs" 
