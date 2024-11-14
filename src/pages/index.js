58179cc653ebac9f3665d7063cb71c27657af3df
Leonardo-Valtorta <leonardovaltorta@gmail.com>
2024-11-14 13:54:41 -0300

    Se agregaron los títulos de cada página en la ficha del navegador (falta product y cart)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import HomeSection from "@/components/HomeSection";
+import Head from "next/head";
 
 
 // const DynamicCarousel = dynamic(() => import('@/components/Carousel'), { ssr: false });
 export default function Home() {
   return (
     <>
-
+      <Head>
+        <title>Código Café - Inicio</title>
+        <meta name="description" content="Esta es la página de inicio" />
+      </Head>
 
       <Header />
       <HomeSection />
