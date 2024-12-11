03c5491317631bfd22f9aace9c72013617bce7e5
CarolinaKegalj <carokegalj@gmail.com>
2024-12-11 10:56:28 -0300

    modificaciones del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,42 +5,7 @@ const Footer = () => {
     return (
 
 <footer className="bg-verde rounded-lg shadow m-4 ">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      
-    <ul className="sm:flex items-center mt-auto text-sm font-medium text-white sm:mt-0">
-        <li>
-            <RedSocial
-              imagen="/assets/icono-instagram.png"
-              sitio="#"
-              texto="Instagram"
-            />
-           
-        </li>
-        <li>
-            <RedSocial
-              imagen="/assets/icono-whatsapp.png"
-              sitio="#"
-              texto="WhatsApp"
-            />
-            
-        </li>
-        <li>
-            <RedSocial
-              imagen="/assets/icono-facebook.png"
-              sitio="#"
-              texto="Facebook"
-            />
-           
-           
-        </li>
-        <li>
-            <RedSocial
-              imagen="/assets/icono-twitter-x.png"
-              sitio="#"
-              texto="Twiter"
-            />
-        </li>
-    </ul>
+    <div className=" w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
     <div className= "sm:flex self-end">
     <ul className="sm:flex items-center mt-auto text-sm font-medium text-white sm:mt-0">   
         <li>
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,10 +1,12 @@
 import ShoppingSection from "@/components/cartSection/ShoppingSection";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header"
 export default function Home() {
   return (
     <> 
     <Header/>
     <ShoppingSection/>
+    <Footer/>
     </>
   );
 }
